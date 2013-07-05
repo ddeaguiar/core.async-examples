0391efd3573f8ed6676cf77fd73ec7497e23c26f
Daniel De Aguiar <ddeaguiar@pointslope.com>
2013-07-05 16:12:16 -0400

    Moved simple worker example to a separate file and added source reference.

--- src/example/core.clj
+++ src/example/core.clj
@@ -1,45 +1,8 @@
 (ns example.core
-  (:require [clojure.core.async :as async :refer :all]))
+  (:require [clojure.core.async :as async :refer :all]
+            [example.simple :as s]))
 
 (comment
-  "Work is represented by a map:"
-
-  {:x 1 :y 2}
-
-  ":z represents the output of the work done based on :x and :y"
-
-  {:x 1 :y 2 :z 2}
-  )
-
-(defn worker
-  "A worker go routine."
-  [in out]
-  (go (while true
-        (let [w (<! in)
-              x (:x w)
-              y (:y w)]
-
-          ;; fake work time
-          (Thread/sleep (rand 1000))
-
-          ;; do the work
-          (->> (* x y)
-              (assoc w :z)
-              (>! out))))))
-
-(defn runner
-  "Launcher which spawns n workers. Outputs the work as it is completed."
-  [n]
-  ( let [in  (chan)
-         out (chan)
-         r (range 1 100)
-         num-workers (range n)]
-
-    ;; spawn workers
-    (doseq [_ num-workers] (worker in out))
-
-    ;; send work in
-    (go (doseq [i r] (>! in
-                         {:x i :y (+ i 1)})))
-    ;; receive work done
-    (<!! (go (doseq [_ r] (prn (<! out)))))))
+  ;; Kick of 10 workers to process work.
+  ;; work will be output to the console.
+  (s/runner 10))
---
+++ src/example/simple.clj
@@ -0,0 +1,45 @@
+(ns example.simple
+  (:require [clojure.core.async :as async :refer :all]))
+
+(comment
+  ;; See Rob Pike's Concurrency is not Parallelism talk for
+  ;; the basis of this example (http://talks.golang.org/2012/waza.slide#39)
+
+  ;; Work is represented by a map
+  {:x 1 :y 2}
+
+  ;; :z represents the output of the work done based on :x and :y
+  {:x 1 :y 2 :z 2})
+
+(defn worker
+  "A worker go routine."
+  [in out]
+  (go (while true
+        (let [w (<! in)
+              x (:x w)
+              y (:y w)]
+
+          ;; fake work time
+          (Thread/sleep (rand 1000))
+
+          ;; do the work
+          (->> (* x y)
+              (assoc w :z)
+              (>! out))))))
+
+(defn runner
+  "Launcher which spawns n workers. Outputs the work as it is completed."
+  [n]
+  ( let [in  (chan)
+         out (chan)
+         r (range 1 100)
+         num-workers (range n)]
+
+    ;; spawn workers
+    (doseq [_ num-workers] (worker in out))
+
+    ;; send work in
+    (go (doseq [i r] (>! in
+                         {:x i :y (+ i 1)})))
+    ;; receive work done
+    (<!! (go (doseq [_ r] (prn (<! out)))))))
