b266fa92be1c13e2c9a86c04b55c5e8e31484742
Daniel De Aguiar <ddeaguiar@pointslope.com>
2013-07-05 18:28:58 -0400

    Update core with the balancer sample.

--- src/example/core.clj
+++ src/example/core.clj
@@ -1,8 +1,12 @@
 (ns example.core
   (:require [clojure.core.async :as async :refer :all]
-            [example.simple :as s]))
+            [example.simple :as s]
+            [example.balancer :as b]))
 
 (comment
   ;; Kick of 10 workers to process work.
   ;; work will be output to the console.
-  (s/runner 10))
+  (s/runner 10)
+
+  ;; Kick off a balancer with 10 workers in the pool.
+  (b/runner 5))
